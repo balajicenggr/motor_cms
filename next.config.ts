33bc0166583c56e54a680a1204b1dd440a63f291
BALAJI <lynxcreativespace@gmail.com>
2026-04-15 18:27:11 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = { reactStrictMode: true };
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
